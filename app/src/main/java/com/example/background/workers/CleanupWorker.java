6942497dd364436e5215c2e4115f631a716d86ee
mehdi <mehdi@cefalo.com>
2018-12-31 20:19:45 +0600

    Android Jetpack: Workmanager example

---
+++ app/src/main/java/com/example/background/workers/CleanupWorker.java
@@ -0,0 +1,56 @@
+package com.example.background.workers;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.example.background.Constants;
+
+import java.io.File;
+
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+/**
+ * Copyright (c) 2018 Cefalo AS. All rights reserved.
+ */
+public class CleanupWorker extends Worker {
+
+    private static final String TAG = CleanupWorker.class.getSimpleName();
+
+    public CleanupWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Context applicationContext = getApplicationContext();
+
+        try {
+            File outputDirectory = new File(applicationContext.getFilesDir(),
+                    Constants.OUTPUT_PATH);
+            if (outputDirectory.exists()) {
+                File[] entries = outputDirectory.listFiles();
+                if (entries != null && entries.length > 0) {
+                    for (File entry : entries) {
+                        String name = entry.getName();
+                        if (!TextUtils.isEmpty(name) && name.endsWith(".png")) {
+                            boolean deleted = entry.delete();
+                            Log.i(TAG, String.format("Deleted %s - %s",
+                                    name, deleted));
+                        }
+                    }
+                }
+            }
+
+            return Worker.Result.success();
+        } catch (Exception exception) {
+            Log.e(TAG, "Error cleaning up", exception);
+            return Worker.Result.failure();
+        }
+    }
+}
